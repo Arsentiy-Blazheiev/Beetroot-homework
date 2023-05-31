75e683f9028dbaade52c7561f47fffdc58c8e811
Arsentiy Blazheiev <b.ars@ukr.net>
2023-05-31 14:14:19 +0300

    Lesson_30

---
+++ Lesson_30/task1.sql
@@ -0,0 +1,18 @@
+--CREATE TABLE my_table (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+--ALTER TABLE my_table 
+--RENAME TO renamed_table;
+
+--ALTER TABLE renamed_table 
+--ADD COLUMN email TEXT;
+
+--INSERT INTO renamed_table (name, age, email)
+--VALUES ('Qwerty', 50, 'qwerty@qwerty.com'),
+		--('Qwert2', 30, 'qwerty2@qwerty.com');
+
+		--UPDATE renamed_table 
+--SET age = 40 
+--WHERE name = 'Qwerty';
+
+--DELETE FROM renamed_table 
+--WHERE name = 'Qwerty2';
